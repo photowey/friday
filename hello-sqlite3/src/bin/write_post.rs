a6fad767aa6b25229669a06c76bb8533e42fa97f
photowey <photowey@gmail.com>
2024-02-28 01:02:43 +0800

    feat: Add: 1.add hello-sqlite3 module
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ hello-sqlite3/src/bin/write_post.rs
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::io::{stdin, Read};
+
+use sqlites::*;
+
+fn main() {
+    let connection = &mut establish_connection();
+
+    let mut title = String::new();
+    let mut body = String::new();
+
+    println!("What would you like your title to be?");
+    stdin().read_line(&mut title).unwrap();
+    let title = title.trim_end(); // Remove the trailing newline
+
+    println!(
+        "\nOk! Let's write {} (Press {} when finished)\n",
+        title, EOF
+    );
+    stdin().read_to_string(&mut body).unwrap();
+
+    let post = create_post(connection, title, &body);
+    println!("\nSaved draft {} with id {}", title, post.id);
+}
+
+#[cfg(not(windows))]
+const EOF: &str = "CTRL+D";
+
+#[cfg(windows)]
+const EOF: &str = "CTRL+Z";
---
+++ hello-sqlite3/src/lib.rs
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::env;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+
+use crate::models::{NewPost, Post};
+
+pub mod models;
+pub mod schema;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut SqliteConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
+}
